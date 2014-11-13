866a9b24b9e044b7aa5653d686212dea8c20d966
Nettraction <dev@nettraction.in>
2014-11-13 15:13:31 +0530

    Modified get_captcha_text to accept custom format.

--- MathCaptcha.php
+++ MathCaptcha.php
@@ -1,81 +1,101 @@
 <?php
 
-/** 
+/**
  * PHP-based Simple Math Captcha
  * 
  * @version 1.0
  * @author Nettraction <dev@nettraction.in>
  * 
  */
-Class MathCaptcha{
-  
+Class MathCaptcha {
+
   private $min;
   private $max;
-  
   private $session_var;
-
   private $result;
   private $operand1;
   private $operand2;
   private $operator;
-  
   private $op_symbols = array('+', '-', '*');
-      
-  function __construct($sess_var='math_captcha_result', $min_val=0, $max_val=10) {
-    
-    $this->min = ($min_val<=0) ? 0 : $min_val;
-    $this->max = ($max_val<=$this->min) ? 10 : $max_val;
-    
-    if( !empty($sess_var) ){
+
+  /**
+   * 
+   * @param string $sess_var
+   * @param int $min_val
+   * @param int $max_val
+   */
+  function __construct($sess_var = 'math_captcha_result', $min_val = 0, $max_val = 10) {
+
+    $this->min = ($min_val <= 0) ? 0 : $min_val;
+    $this->max = ($max_val <= $this->min) ? 10 : $max_val;
+
+    if (!empty($sess_var)) {
       $this->session_var = $sess_var;
-    }else{
+    } else {
       $this->session_var = 'math_captcha_result';
     }
-
   }
-  
-  public function reset_captcha(){
-    
+
+  /**
+   * Generate a new captcha and save the result into a session variable. 
+   *
+   */
+  public function reset_captcha() {
+
     $this->operand1 = rand($this->min, $this->max);
     $this->operand2 = rand($this->min, $this->max);
-    $this->operator = $this->op_symbols[rand(0, (count($this->op_symbols)-1))];
-    
+    $this->operator = $this->op_symbols[rand(0, (count($this->op_symbols) - 1))];
+
     $this->compute_result();
-    
+
     // Save to $_SESSION
     $_SESSION[$this->session_var] = $this->result;
   }
-  
+
   private function compute_result() {
-    
-    switch ($this->operator){
+
+    switch ($this->operator) {
       case '+':
         $this->result = ($this->operand1 + $this->operand2);
         break;
-      
+
       case '-':
         $this->result = ($this->operand1 - $this->operand2);
         break;
-      
+
       case '*':
         $this->result = ($this->operand1 * $this->operand2);
         break;
     }
   }
-  
-  public function validate($val){
-    if( $val == $_SESSION[$this->session_var] ){
+
+  /**
+   * 
+   * @param int $val Value to be compared to the result in session
+   * @return boolean TRUE if the value matches; FALSE otherwise
+   */
+  public function validate($val) {
+    if ($val == $_SESSION[$this->session_var]) {
       return TRUE;
-    }else{
+    } else {
       return FALSE;
     }
   }
-  
-  public function get_captcha_text($format='%d %s %d'){
-    if(!empty($format)){
-      return sprintf($format, $this->operand1, $this->operator, $this->operand2);
-    }else{
+
+  /**
+   * 
+   * @param string $format sprintf compatible format with text/html e.g "Compute Sum of {operand1} and {operand2}"
+   * @return type
+   */
+  public function get_captcha_text($format = '{operand1} {operator} {operand2}') {
+    if (!empty($format)) {
+      return str_replace(
+          array('{operand1}', '{operand2}', '{operator}')
+          , array($this->operand1, $this->operand2, $this->operator)
+          , $format);
+    } else {
       return sprintf("%d %s %d", $this->operand1, $this->operator, $this->operand2);
     }
   }
+
 }
